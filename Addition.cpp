b794156ea40753c4bf07989cafdf690f9fb82029
Isayasurusi <ijonesreuzy@gmail.com>
2024-12-26 22:47:22 +0300

    Add files via upload

---
+++ Addition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x;
+	int y;
+	
+	cout<<"Enter First Number: ";
+	cin>>x;
+	cout<<"Enter Second Number: ";
+	cin>>y;
+	
+	cout<<"Result is: "<<x+y;
+
+    return 0;
+}
+
